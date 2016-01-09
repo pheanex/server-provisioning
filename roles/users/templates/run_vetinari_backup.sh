486092d5cfab5b91a60bd0468c18c34dc3c796cf
Konstantin Manna <Konstantin@Manna.uno>
2016-01-09 21:49:13 +0100

    changed backup mirror synch from git to rsync

--- roles/users/templates/run_vetinari_backup.sh
+++ roles/users/templates/run_vetinari_backup.sh
@@ -3,28 +3,11 @@
 /home/pheanex/bin/mount_vetinari_backup.sh &&
 for directory in Bilder Dokumente Studium
 do
-	# Check if git repo exists or create if not
-	cd "/home/pheanex/Backup/${directory}" &&
-	if ! git rev-parse --is-inside-work-tree &> /dev/null
-	then
-		git init --bare . &&
-		git remote add vetinari "/home/pheanex/backup_decrypted/${directory,,}/"
-	fi &&
-	# Synch repo with vetinari
-	git fetch --all &&
-	git push -u vetinari --all
+	rsync -crlpEADPXv "${HOME}/${directory}" "${HOME}/backup_decrypted/${directory}"
 done &&
-# Check if git repo exists or create if not
-cd /home/pheanex/maildir &&
-if ! git rev-parse --is-inside-work-tree &> /dev/null
-then
-	git init . &&
-	git remote add vetinari "/home/pheanex/backup_decrypted/mail"
-fi &&
-git fetch --all &&
-git checkout master &&
+cd "${HOME}/maildir" &&
 git add -A . &&
 git commit -m "update from $(date)" &&
-git push
+rsync -crlpEADPXv "${HOME}/maildir/" "${HOME}/backup_decrypted/maildir"
 
 /home/pheanex/bin/umount_vetinari_backup.sh
