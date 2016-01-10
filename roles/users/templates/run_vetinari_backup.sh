a9d5cc917a53c9add4766d8b99d5f5e81cdc8765
Konstantin Manna <Konstantin@Manna.uno>
2016-01-10 16:43:39 +0100

    bugfix: no Bilder/bilder any longer, just bilder/

--- roles/users/templates/run_vetinari_backup.sh
+++ roles/users/templates/run_vetinari_backup.sh
@@ -3,11 +3,11 @@
 /home/pheanex/bin/mount_vetinari_backup.sh &&
 for directory in Bilder Dokumente Studium
 do
-	rsync -crlpEADPXv "${HOME}/Backup/${directory}" "${HOME}/backup_decrypted/${directory}"
+	rsync -crlpEADPXv "${HOME}/Backup/${directory}" "${HOME}/backup_decrypted/"
 done &&
 cd "${HOME}/maildir" &&
 git add -A . &&
 git commit -m "update from $(date)" &&
-rsync -crlpEADPXv "${HOME}/maildir/" "${HOME}/backup_decrypted/maildir"
+rsync -crlpEADPXv "${HOME}/maildir/" "${HOME}/backup_decrypted/"
 
 /home/pheanex/bin/umount_vetinari_backup.sh
