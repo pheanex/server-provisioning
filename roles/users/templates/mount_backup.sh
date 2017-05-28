c79f4ecffe75edc98817078197d1b3284ac3149b
Konstantin Manna <Konstantin@Manna.uno>
2017-05-28 16:55:00 +0200

    add missing port parameter to sshfs

--- roles/users/templates/mount_backup.sh
+++ roles/users/templates/mount_backup.sh
@@ -19,7 +19,7 @@ fi
 mkdir "$sshfs_local_dir" "$encfs_decrypted_dir"
 
 # Mount SSHFS
-sshfs "${sshfs_remote_user}@${sshfs_remote_host}:${sshfs_remote_dir}" "$sshfs_local_dir" -o IdentityFile="$sshfs_remote_user_key" -o uid="$(id -u $(whoami))" -o gid="$(id -g $(whoami))" &&
+sshfs "${sshfs_remote_user}@${sshfs_remote_host}:${sshfs_remote_dir}" "$sshfs_local_dir" -o IdentityFile="$sshfs_remote_user_key" -o uid="$(id -u $(whoami))" -o gid="$(id -g $(whoami))" -o port="${sshfs_remote_port}" &&
 
 # Mount EncFS
 echo "$password" | encfs "$sshfs_local_dir" "$encfs_decrypted_dir" -S
