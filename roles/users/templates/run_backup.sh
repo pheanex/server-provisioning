6cdc7d70457f38bd820bed43ca595d8537289487
Konstantin Manna <Konstantin@Manna.uno>
2017-05-30 17:38:40 +0200

    use cp instead of rsync for backup
    its way faster that way

--- roles/users/templates/run_backup.sh
+++ roles/users/templates/run_backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+cd "${HOME}/maildir" &&
+git add -A . &&
+git commit -m "update from $(date)" &&
+cd - &&
+cp -R maildir/ Backup/ &&
 for backup_location in vetinari raspberry
 do
 	/home/pheanex/bin/mount_backup.sh "$backup_location" &&
-	for directory in Bilder Dokumente Studium
+	for dir in Backup/*
 	do
-		rsync -crl --partial "${HOME}/Backup/${directory}" "${HOME}/backup_decrypted/"
-	done &&
-	cd "${HOME}/maildir" &&
-	git add -A . &&
-	git commit -m "update from $(date)" &&
-	rsync -crl --partial "${HOME}/maildir/" "${HOME}/backup_decrypted/"
-
+		cp -Ruv "$dir" backup_decrypted/
+	done
 	/home/pheanex/bin/umount_backup.sh "$backup_location"
 done
