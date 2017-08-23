8ae62ecf727d59d30cffc6e53d6082fba4180a6d
Konstantin Manna <Konstantin@Manna.uno>
2017-08-23 22:45:31 +0200

    implement nextcloud backup

--- roles/users/templates/run_backup.sh
+++ roles/users/templates/run_backup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-cd "${HOME}/maildir" &&
-git add -A . &&
-git commit -m "update from $(date)"
-cd -
-for backup_location in vetinari raspberry
+db_dump_file="/tmp/nextcloud-db-dump.sql"
+backup_dir="backup_decrypted/"
+
+mysqldump --single-transaction -u {{nextcloud_db_admin}} -p{{nextcloud_db_pwd}} {{nextcloud_db_name}} > "$db_dump_file"
+
+
+for location in raspberry vetinari
 do
-	/home/pheanex/bin/mount_backup.sh "$backup_location" &&
-	for dir in Backup/* maildir
-	do
-		cp -Ruv "$dir" backup_decrypted/
-	done
-	/home/pheanex/bin/umount_backup.sh "$backup_location"
+	/home/pheanex/bin/mount_backup.sh "$location" || break
+	cp -uv "$db_dump_file" "$backup_dir"nextcloud-db-dump.sql_new && mv -v "$backup_dir"nextcloud-db-dump.sql_new "$backup_dir"nextcloud-db-dump.sql
+	cp -Ruv /home/nextcloud/ncdata/ "$backup_dir"
+	cp -Ruv /home/pheanex/maildir/ "$backup_dir"
+	/home/pheanex/bin/umount_backup.sh "$location" || exit 1
 done
+
+rm -f "$db_dump_file"
